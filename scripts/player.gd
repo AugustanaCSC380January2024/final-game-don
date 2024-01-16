2802678f3f7c687f42a241cd828f76a5586b21e6
kevin <rodriguezkn60@gmail.com>
2024-01-15 20:51:46 -0600

    Fixed sprite animation bugs

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,30 +2,47 @@ extends CharacterBody2D
 
 @onready var tile_map = $"../map1/TileMap"
 @onready var animated_sprite = $AnimatedSprite2D
-@export var speed = 125
+
+@export var jumpSpeed = 250
+@export var normalSpeed = 125
 
 func _physics_process(delta):
 	
 	var directionX = 0
 	var directionY = 0
+	var speed = normalSpeed
+	var jump = false
 	
 	directionX = Input.get_axis("left", "right")
 	directionY = Input.get_axis("up", "down")
+	if Input.is_action_pressed("jump"):
+		speed = jumpSpeed
+		jump = true
+		
+	else:
+		speed = normalSpeed
+		jump = false
+		
 	
 	if directionX != 0:
 		animated_sprite.flip_h = (directionX == -1)
 		
 	velocity.x = directionX * speed
 	velocity.y = directionY * speed
-	update_animations(directionX, directionY)
+	
+	update_animations(directionX, directionY, jump)
 	move_and_slide()
 	
 
-func update_animations(directionX, directionY):
-	if(directionX == 0 && directionY == 0 ):
+func update_animations(directionX, directionY, jump):
+	if(jump == true):
+		animated_sprite.play("jump")
+	elif(directionX == 0 && directionY == 0):
 		animated_sprite.play("idle")
-	else: 
+	elif (directionX != 0 || directionY != 0): 
 		animated_sprite.play("run")
-		
+	
+	
+
 		
 	
