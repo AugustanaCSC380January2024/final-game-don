4550691abe4e0f28d223b5865e000c765bff9167
kevin <rodriguezkn60@gmail.com>
2024-01-15 20:21:11 -0600

    We added animated sprite functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,31 @@
 extends CharacterBody2D
 
 @onready var tile_map = $"../map1/TileMap"
+@onready var animated_sprite = $AnimatedSprite2D
+@export var speed = 125
 
 func _physics_process(delta):
-	velocity = Vector2(0,0)
-	if Input.is_action_pressed("up"):
-		velocity.y = -200
-	if Input.is_action_pressed("down"):
-		velocity.y = 200
-	if Input.is_action_pressed("right"):
-		velocity.x = 200
-	if Input.is_action_pressed("left"):
-		velocity.x = -200
+	
+	var directionX = 0
+	var directionY = 0
+	
+	directionX = Input.get_axis("left", "right")
+	directionY = Input.get_axis("up", "down")
+	
+	if directionX != 0:
+		animated_sprite.flip_h = (directionX == -1)
 		
+	velocity.x = directionX * speed
+	velocity.y = directionY * speed
+	update_animations(directionX, directionY)
 	move_and_slide()
 	
-	#global_position = global_position.clamp(Vector2(0,0), scree)
-		
 
+func update_animations(directionX, directionY):
+	if(directionX == 0 && directionY == 0 ):
+		animated_sprite.play("idle")
+	else: 
+		animated_sprite.play("run")
+		
 		
 	
