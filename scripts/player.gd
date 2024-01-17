3ea18de08fa17f4253ca69d7bb21de1abfe70ede
aslaoui03 <144163544+aslaoui03@users.noreply.github.com>
2024-01-16 18:08:27 -0600

    attempted to give delays between jumps

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var tile_map = $"../map1/TileMap"
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var on_floor = true
+@onready var timer = $Timer
 
 @export var jumpSpeed = 250
 @export var normalSpeed = 125
@@ -15,15 +17,20 @@ func _physics_process(delta):
 	
 	directionX = Input.get_axis("left", "right")
 	directionY = Input.get_axis("up", "down")
-	if Input.is_action_pressed("jump"):
+	
+	
+	
+	if (Input.is_action_pressed("jump")):
+		print("Jumping!")
 		speed = jumpSpeed
 		jump = true
-		
+		on_floor = false
+		timer.start()
+
 	else:
 		speed = normalSpeed
 		jump = false
 		
-	
 	if directionX != 0:
 		animated_sprite.flip_h = (directionX == -1)
 		
@@ -33,16 +40,17 @@ func _physics_process(delta):
 	update_animations(directionX, directionY, jump)
 	move_and_slide()
 	
-
 func update_animations(directionX, directionY, jump):
 	if(jump == true):
-		animated_sprite.play("jump")
+		animated_sprite.play("jump")	
+		
 	elif(directionX == 0 && directionY == 0):
 		animated_sprite.play("idle")
 	elif (directionX != 0 || directionY != 0): 
 		animated_sprite.play("run")
-	
-	
 
-		
+
+func _on_timer_timeout():
+	on_floor = true
+	timer.start()
 	
