10405057a2ddf58f0b37cbc423fd523058d8817a
Kevin <rodriguezkn60@gmail.com>
2024-01-16 20:24:45 -0600

    Added enemy with animated sprite and follows player functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 @onready var tile_map = $"../map1/TileMap"
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var on_floor = true
-@onready var timer = $Timer
-
 @export var jumpSpeed = 250
 @export var normalSpeed = 125
+var my_timer : Timer
+
 
 func _physics_process(delta):
 	
@@ -17,20 +17,17 @@ func _physics_process(delta):
 	
 	directionX = Input.get_axis("left", "right")
 	directionY = Input.get_axis("up", "down")
-	
-	
-	
-	if (Input.is_action_pressed("jump")):
-		print("Jumping!")
+	if Input.is_action_pressed("jump"):
+		$Timer.start()
 		speed = jumpSpeed
 		jump = true
 		on_floor = false
-		timer.start()
-
+		
 	else:
 		speed = normalSpeed
 		jump = false
 		
+	
 	if directionX != 0:
 		animated_sprite.flip_h = (directionX == -1)
 		
@@ -39,18 +36,27 @@ func _physics_process(delta):
 	
 	update_animations(directionX, directionY, jump)
 	move_and_slide()
+	print(on_floor)
 	
+  
+	
+
 func update_animations(directionX, directionY, jump):
 	if(jump == true):
-		animated_sprite.play("jump")	
-		
+		animated_sprite.play("jump")
+		get_tree().create_timer(1.0).timeout
 	elif(directionX == 0 && directionY == 0):
 		animated_sprite.play("idle")
 	elif (directionX != 0 || directionY != 0): 
 		animated_sprite.play("run")
+	
+	
 
+		
+	
 
+#notes, timer works but it seems like there is not enough time for the animation to play
 func _on_timer_timeout():
 	on_floor = true
-	timer.start()
+	print("timeout")
 	
